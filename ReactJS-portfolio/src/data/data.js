6cbd10e53a84320571b5803f52c9b199137d1fd2
vikamabuda <vikamabuda900@gmail.com>
2023-12-01 14:33:25 +0200

    first commit

---
+++ ReactJS-portfolio/src/data/data.js
@@ -0,0 +1,31 @@
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe} from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
+
+// Features Data
+export const featuresData = [
+  {
+    id: 1,
+    title: "Software Development",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 2,
+    icon: <AiFillAppstore />,
+    title: "App Development",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 3,
+    icon: <SiProgress />,
+    title: "Web Development",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  {
+    id: 4,
+    icon: <FaMobile />,
+    title: "Mobile Development",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+  
+];
